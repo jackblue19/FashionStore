85debeda747e335f3b7f51a9267df86bd874038b
Jack Blue <git.jack1624@gmail.com>
2025-06-24 00:29:31 +0700

    Clean Architecture MVVM

---
+++ app/src/main/java/com/example/fashionstore/ui/forgot/ForgotPasswordActivity.java
@@ -0,0 +1,4 @@
+package com.example.fashionstore.ui.forgot;
+
+public class ForgotPasswordActivity {
+}
---
+++ app/src/main/java/com/example/fashionstore/ui/login/LoginViewModel.java
@@ -0,0 +1,4 @@
+package com.example.fashionstore.ui.login;
+
+public class LoginViewModel {
+}
