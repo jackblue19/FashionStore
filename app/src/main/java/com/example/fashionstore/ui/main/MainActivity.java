5273f8e2700ae8f78f2977c686ba0cdad7d1b9de
Jack Blue <git.jack1624@gmail.com>
2025-06-24 13:45:03 +0700

    before gg

--- app/src/main/java/com/example/fashionstore/ui/main/MainActivity.java
+++ app/src/main/java/com/example/fashionstore/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.fashionstore.ui.main;
 
+import android.util.Log;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,18 +10,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fashionstore.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "DEBUG_FIREBASE";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        FirebaseApp.initializeApp(this);
+
+        // Lấy instance để kích hoạt FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        Log.d(TAG, "Firebase initialized and FirebaseAuth.getInstance() called.");
+
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
